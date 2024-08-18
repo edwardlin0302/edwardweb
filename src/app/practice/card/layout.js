558c1317f361bfade849d585e7f543c1e060273b
edwardlin0302 <edwardlin0302@gmail.com>
2024-08-18 21:58:33 +0800

    add navbar and router

---
+++ src/app/practice/card/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const layout = ({ children }) => {
+    return <section>{children}</section>;
+};
+
+export default layout;
---
+++ src/app/practice/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Practice() {
+    return (
+        <main>
+            <Link href="/practice/card">card</Link>
+        </main>
+    );
+}
